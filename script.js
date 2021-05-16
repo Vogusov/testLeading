226929d891b2933337c597a386da885ff02d172c
Vogusov <vogusov@gmail.com>
2021-05-16 15:30:27 +0300

    JS: событие нанжатия стрелок теперь однократно

--- script.js
+++ script.js
@@ -1,17 +1,44 @@
-$('body').keydown(function (event) {
-  let keyPressedCode = event.keyCode
-  switch (keyPressedCode) {
-    case 37:
-      alert('Нажата клавиша влево')
-      break;
-    case 38:
-      alert('Нажата клавиша вверх')
-      break;
-    case 39:
-      alert('Нажата клавиша вправо')
-      break;
-    case 40:
-      alert('Нажата клавиша вниз')
-      break;
-  }
+$(document).ready(function() {
+
+  const pressedKeys = [];
+
+  $('body').bind('keydown', (function (event) {
+    let keyPressedCode = event.keyCode
+    // console.log(event)
+    switch (keyPressedCode) {
+
+      case 37:
+        if (jQuery.inArray(keyPressedCode, pressedKeys) >= 0)
+          break;
+
+        alert('Нажата клавиша влево')
+        pressedKeys.push(keyPressedCode)
+        break;
+
+      case 38:
+        if (jQuery.inArray(keyPressedCode, pressedKeys) >= 0)
+          break;
+
+        alert('Нажата клавиша вверх')
+        pressedKeys.push(keyPressedCode)
+        break;
+
+      case 39:
+        if (jQuery.inArray(keyPressedCode, pressedKeys) >= 0)
+          break;
+
+        alert('Нажата клавиша вправо')
+        pressedKeys.push(keyPressedCode)
+        break;
+
+      case 40:
+        if (jQuery.inArray(keyPressedCode, pressedKeys) >= 0)
+          break;
+
+        alert('Нажата клавиша вниз')
+        pressedKeys.push(keyPressedCode)
+        break;
+    }
+  }))
+
 })
